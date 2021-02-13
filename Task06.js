f4dc740274d448424095a576884048b4c3514081
Christopher Maphakane <Christopher.Maphakane@gijima.com>
2021-02-13 09:48:45 +0200

    used one function as requested

--- Task06.js
+++ Task06.js
@@ -1,18 +1,18 @@
-var num1 = maximumNumber(2,25,16);
+//var num1 = maximumNumber(2,25,16);
 
-function maximumNumber(num1, num2, num3) { //checking maximum number from 3 numbers  
-    var max = num1;
-    if (num2 > max) {
-        max = num2;
-    }
-    if (num3 > max) {
-        max = num3;
-    }
-    return max;
-}
+//function maximumNumber(num1, num2, num3) { //checking maximum number from 3 numbers  
+  //  var max = num1;
+   // if (num2 > max) {
+ //       max = num2;
+ //   }
+//    if (num3 > max) {
+ //       max = num3;
+ //   }
+ //   return max;
+//}
 
-var num2 = maxOfInfinity(8,52,15,46,334);
-function maxOfInfinity() {
+var num1 = maxOfMultipleNumbers(8,525,615,46,334);
+function maxOfMultipleNumbers(num1) {
     var max = num1;
 
     for (i = 0; i < arguments.length; i++)
