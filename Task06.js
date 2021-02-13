73f77625ecffe17e29bcbaffb066d5c293fbc907
Christopher Maphakane <Christopher.Maphakane@gijima.com>
2021-02-13 09:49:58 +0200

    codes modified as requested on review

--- Task06.js
+++ Task06.js
@@ -1,16 +1,3 @@
-//var num1 = maximumNumber(2,25,16);
-
-//function maximumNumber(num1, num2, num3) { //checking maximum number from 3 numbers  
-  //  var max = num1;
-   // if (num2 > max) {
- //       max = num2;
- //   }
-//    if (num3 > max) {
- //       max = num3;
- //   }
- //   return max;
-//}
-
 var num1 = maxOfMultipleNumbers(8,525,615,46,334);
 function maxOfMultipleNumbers(num1) {
     var max = num1;
--- Task10.js
+++ Task10.js
@@ -1,14 +1,12 @@
 function commonLetters(string1,string2){
-
-    for(let i = 0; i < string1.length; i++){  
-       for (let j = 0; j < string2.length; j++){
-           var letter1 = string1[i];
-           var letter2 = string2[j];
-           if(letter1.toUpperCase() == letter2.toUpperCase()){ //converting characters in a string to uppercase.
-                console.log(letter1);
-           }
-        }
-       }
-       
-       
-}commonLetters("CHRIS","christopher");
+    var str1 = string1.toLowerCase();
+    var str2 = string2.toLowerCase();
+    
+          for (var i = 0; i < str1.length; i++){
+              if(str2.indexOf(str1[i]) != -1){ //checking for common characters in two strings
+                console.log(str1[i]);
+               
+             }
+            }
+        
+   }commonLetters("chrisen","CHRISTOPHER");
